1bb613cb67b3050add5450b3a5bcc0bbab505202
riyanhossan <73151396+riyanhossan@users.noreply.github.com>
2021-01-07 10:48:18 +0600

    Add files via upload

---
+++ JS-JQ/js/script.js
@@ -0,0 +1,22 @@
+// js output
+
+// alert("All is Ok.");
+// document.write("All is ok.");
+// innerHTML='';
+// console.log("All is OK");
+
+// document.getElementById("result").innerHTML="All is OK.";
+
+
+// JS Variable:
+	var number1= true;
+	var number2 = 20;
+	
+var data= typeof(number1);
+	
+document.write(data);
+
+
+
+
+	
